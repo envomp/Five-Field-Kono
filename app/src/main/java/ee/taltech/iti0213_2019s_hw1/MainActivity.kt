23bdbae80cd5ec0bc11c1db8bda855dd1fdae377
envomp <envomp@ttu.ee>
2020-02-18 00:24:02 +0200

    UI

--- app/src/main/java/ee/taltech/iti0213_2019s_hw1/MainActivity.kt
+++ app/src/main/java/ee/taltech/iti0213_2019s_hw1/MainActivity.kt
@@ -2,11 +2,31 @@ package ee.taltech.iti0213_2019s_hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = this::class.java.declaringClass!!.simpleName
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner_mode_selection)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.game_modes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
     }
 }
